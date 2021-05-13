4ce3ef15d47d671c81c6369a1f2f00764a2a72ea
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-05-13 21:19:02 +0300

    Done!

--- Coins/app/src/main/java/com/omercankoc/coins/service/CoinAPI.kt
+++ Coins/app/src/main/java/com/omercankoc/coins/service/CoinAPI.kt
@@ -1,7 +1,9 @@
 package com.omercankoc.coins.service
 
 import android.telecom.Call
+import io.reactivex.Observable
 import retrofit2.http.GET
+import java.util.*
 import com.omercankoc.coins.model.CoinModel as CoinModel
 
 interface CoinAPI {
@@ -10,8 +12,13 @@ interface CoinAPI {
 
     // GET:
     @GET("prices?key=14305767c0e2b4e57f6fd6bfc0b5bf283273415b")
+
+    //RxJava ile verileri indir.
+    fun getData() : Observable<List<CoinModel>>
+
     // Main Thread bloklamadan (Async) verileri indir.
-    fun getData(): retrofit2.Call<List<CoinModel>>
+    //fun getData(): retrofit2.Call<List<CoinModel>>
+
     // POST:
 
     // UPDATE:
--- Coins/app/src/main/java/com/omercankoc/coins/view/MainActivity.kt
+++ Coins/app/src/main/java/com/omercankoc/coins/view/MainActivity.kt
@@ -2,26 +2,47 @@ package com.omercankoc.coins.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.omercankoc.coins.R
+import com.omercankoc.coins.adapter.RecyclerViewAdapter
 import com.omercankoc.coins.model.CoinModel
 import com.omercankoc.coins.service.CoinAPI
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.schedulers.Schedulers.io
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
 
     // API KEY : 14305767c0e2b4e57f6fd6bfc0b5bf283273415b
     // URL : curl "https://api.nomics.com/v1/prices?key=14305767c0e2b4e57f6fd6bfc0b5bf283273415b"
 
     private val BASE_URL = "https://api.nomics.com/v1/"
     private var coinModels : ArrayList<CoinModel>? = null
+    private var recyclerViewAdapter : RecyclerViewAdapter? = null
+
+    // Disposable : Tek kullanimlik istekler.
+    private var compositeDisposable : CompositeDisposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        compositeDisposable = CompositeDisposable()
+
+        // Recycle View tanimla.
+        val layoutManager : RecyclerView.LayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
 
         downloadData()
     }
@@ -30,13 +51,19 @@ class MainActivity : AppCompatActivity() {
         // Retrofit Object olustur.
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create()) // RxJava ile...
             .addConverterFactory(GsonConverterFactory.create())
-            .build()
+            .build().create(CoinAPI::class.java)
 
+        compositeDisposable?.add(retrofit.getData()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::handleResponse))
+
+        /* Retrofit ile...
         // Service ile Retrofit'i birbirine bagla.
         val service = retrofit.create(CoinAPI::class.java)
         val call = service.getData()
-
         // Async sekilde verileri al.
         call.enqueue(object : Callback<List<CoinModel>> {
             // Cevap gelir ise...
@@ -50,10 +77,17 @@ class MainActivity : AppCompatActivity() {
                     response.body()?.let {
                         coinModels = ArrayList(it)
 
+                        /*
                         for (coinModel : CoinModel in coinModels!!){
                             println(coinModel.currency)
                             println(coinModel.price)
+                        } */
+
+                        coinModels?.let {
+                            recyclerViewAdapter = RecyclerViewAdapter(coinModels!!,this@MainActivity)
+                            recyclerView.adapter = recyclerViewAdapter
                         }
+
                     }
                 }
             }
@@ -62,6 +96,26 @@ class MainActivity : AppCompatActivity() {
             override fun onFailure(call: Call<List<CoinModel>>, t: Throwable) {
                 t.printStackTrace()
             }
-        })
+        }) */
+    }
+
+    // Bos degil ise, RxJava ile Async sekilde verileri al.
+    private fun handleResponse(coinList: List<CoinModel>){
+        coinModels = ArrayList(coinList)
+
+        coinModels?.let {
+            recyclerViewAdapter = RecyclerViewAdapter(it,this@MainActivity)
+            recyclerView.adapter = recyclerViewAdapter
+        }
+    }
+
+    override fun onItemClick(coinModel: CoinModel) {
+        Toast.makeText(this,"Clicked : ${coinModel.currency}",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        // Temizle...
+        compositeDisposable?.clear()
+        super.onDestroy()
     }
 }
