15de7ed30b2732322ab1594fe8374d43172f0532
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-08-21 08:37:37 +0300

    Delete Coins directory

--- Coins/app/src/main/java/com/omercankoc/coins/model/CoinModel.kt
+++
@@ -1,9 +0,0 @@
-package com.omercankoc.coins.model
-
-// Data sinifinda model olustur.
-data class CoinModel(
-    //@SerializedName("currency")
-    val currency : String,
-    //@SerializedName("price")
-    val price : String
-    )
