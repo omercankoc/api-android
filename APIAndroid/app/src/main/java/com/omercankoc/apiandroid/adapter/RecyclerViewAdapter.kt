f937f1a0951ef90ed77022a16d47476c3a3b520b
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-08-21 15:41:33 +0300

    Update UIs

--- APIAndroid/app/src/main/java/com/omercankoc/apiandroid/adapter/RecyclerViewAdapter.kt
+++ APIAndroid/app/src/main/java/com/omercankoc/apiandroid/adapter/RecyclerViewAdapter.kt
@@ -3,17 +3,17 @@ package com.omercankoc.apiandroid.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.omercankoc.apiandroid.databinding.RecycleViewRowBinding
+import com.omercankoc.apiandroid.databinding.RecyclerViewRowBinding
 import com.omercankoc.apiandroid.model.Coin
 
 class RecyclerViewAdapter(private val coinList : ArrayList<Coin>) : RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewRowHolder>() {
 
     // Ilgili view'in tutucu nesnesi.
-    class RecyclerViewRowHolder(val binding : RecycleViewRowBinding) : RecyclerView.ViewHolder(binding.root){ }
+    class RecyclerViewRowHolder(val binding : RecyclerViewRowBinding) : RecyclerView.ViewHolder(binding.root){ }
 
     // View holder olusturuldugunda layout'un view'larini bagla.
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewRowHolder {
-        val binding = RecycleViewRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = RecyclerViewRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return RecyclerViewRowHolder(binding)
     }
 
